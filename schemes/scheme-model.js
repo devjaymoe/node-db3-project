3db9ca28bb558cba9e7780feb017bce34730572f
Devin_Moore <d.j.moore209@gmail.com>
2020-05-13 17:58:37 -0400

    all endpoints for mvp complete

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -11,29 +11,46 @@ module.exports = {
 }
 
 function find() {
-
+  return db('schemes')
 }
 
 function findById(id) {
-
-}
-
-function findSteps(id) {
-
+  return db('schemes').where({ id }).first();
 }
 
 function add(data) {
-
-}
-
-function addStep(stepData, id) {
-
+  return db('schemes')
+    .insert(data)
+    .then(ids => {
+      return findById(ids[0]);
+    });
 }
 
 function update(changes, id) {
-
+  return db('schemes')
+    .where({ id })
+    .update(changes)
+    .then(success => {
+      if(success) {
+        return findById(id)
+      } else {
+        return res.status(500).json({ message: 'Failed to update scheme' });
+      }
+    })
 }
 
 function remove(id) {
+  return db('schemes').where({ id }).del();
+}
 
+function findSteps(id) {
+  return db('schemes as sc')
+    .join('steps as s', 's.scheme_id', '=', 'sc.id')
+    .select('sc.scheme_name', 's.step_number', 's.instructions')
+    .where('sc.id', '=', id)
+    .orderBy('s.step_number')
+}
+
+function addStep(stepData, id) {
+  return db('schemes')
 }
