1a67c9fb4f098472c5bdb64968324a2d319768e8
Devin_Moore <d.j.moore209@gmail.com>
2020-05-13 16:41:26 -0400

    added data config and module layout for functions

---
+++ schemes/scheme-model.js
@@ -0,0 +1,39 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  addStep,
+  update,
+  remove
+}
+
+function find() {
+
+}
+
+function findById(id) {
+
+}
+
+function findSteps(id) {
+
+}
+
+function add(data) {
+
+}
+
+function addStep(stepData, id) {
+
+}
+
+function update(changes, id) {
+
+}
+
+function remove(id) {
+
+}
